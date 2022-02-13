cc5b34b094508f6c6234a1b8a1625ad574167e09
pawel0705 <pawel.salicki@gmail.com>
2022-02-13 11:26:15 +0100

    Sketching mechanism and connection to python server (WIP)

---
+++ TerrainCreator/Assets/Scripts/PixelHandler.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PixelHandler : MonoBehaviour
+{
+    private Image image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public float GetColor()
+    {
+        return image.color.r;
+    }
+
+    public void ChanePixelColor(float color)
+    {
+        if (color >= 1)
+        {
+            color = 1;
+        }
+        else if (color <= 0)
+        {
+            color = 0;
+        }
+
+        image.color = new Color(color, color, color);
+    }
+}
---
+++ TerrainCreator/Assets/Scripts/PredictionRequester.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using AsyncIO;
+using NetMQ;
+using NetMQ.Sockets;
+using UnityEngine;
+
+public class PredictionRequester : RunAbleThread
+{
+    public RequestSocket client;
+
+    private Action<float[]> onOutputReceived;
+    private Action<Exception> onFail;
+
+    protected override void Run()
+    {
+        ForceDotNet.Force(); // this line is needed to prevent unity freeze after one use, not sure why yet
+
+        using (RequestSocket client = new RequestSocket())
+        {
+            this.client = client;
+            client.Connect("tcp://localhost:5555");
+
+            while (Running)
+            {
+                byte[] outputBytes = new byte[0];
+                bool gotMessage = false;
+
+                while (Running)
+                {
+
+                    try
+                    {
+                        gotMessage = client.TryReceiveFrameBytes(out outputBytes);  // this returns true if it's successful
+
+                        if (gotMessage)
+                        {
+                            break;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.Log(e.Message);
+                    }
+
+                }
+
+                if (gotMessage)
+                {
+                    var output = new float[outputBytes.Length / 4];
+                    Buffer.BlockCopy(outputBytes, 0, output, 0, outputBytes.Length);
+                    onOutputReceived?.Invoke(output);
+                }
+            }
+        }
+
+        NetMQConfig.Cleanup(); // this line is needed to prevent unity freeze after one use, not sure why yet
+    }
+
+    public void SendInput(float[] input)
+    {
+        Debug.Log(input.Length);
+        try
+        {
+            var byteArray = new byte[input.Length * 4];
+            Buffer.BlockCopy(input, 0, byteArray, 0, byteArray.Length);
+            client.SendFrame(byteArray);
+        }
+        catch (Exception ex)
+        {
+            onFail(ex);
+        }
+    }
+
+    public void SetOnTextReceivedListener(Action<float[]> onOutputReceived, Action<Exception> fallback)
+    {
+        this.onOutputReceived = onOutputReceived;
+        onFail = fallback;
+    }
+}
